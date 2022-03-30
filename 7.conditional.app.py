7f64a58ff6fec4187660a59b7a2c7e52ff13fdc0
Monic88 <102010562+Monic88@users.noreply.github.com>
2022-03-30 15:58:43 +0900

    flask 추가

--- 7.conditional.app.py
+++ 7.conditional.app.py
@@ -2,7 +2,7 @@ input_id = input('아이디를 입력해주세요. ')
 input_pwd = input('비밀번호를 입력해주세요. ')
 if input_id == 'egoing' :
     if input_pwd == '8842':
-        print('안녕하세e요')
+        print('안녕하세요')
     else:
         print('비밀번호가 다릅니다')
 else:
